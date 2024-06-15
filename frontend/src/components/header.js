9a6713dfd4983e76c6d0ceda75b4842b447665a8
SimranRojia <149954788+SimranRojia@users.noreply.github.com>
2024-06-15 23:56:44 +0530

    website nav bar

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -3,55 +3,22 @@ import './global.css';
 import './profile-screen.css';
 import groupImage from '../assets/Logo.png';
 import userimage from '../assets/user.png';
+import './P.css';
+
 
 function Header() {
-  const navStyle = {
-    display: 'flex',
-    
-    justifyContent: 'space-between',
-    padding: '10px 20px',
-    backgroundColor: '#f8f8f8',
-    marginTop: '5px' ,
-    position: 'fixed',
-    top: '0',
-    width: '100%',
-    zIndex: '1000',
-    left: '0',
-    
-
-  };
-
-  const imgStyle = {
-    width: '50px',
-    height: 'auto'
-  };
-
-  const textStyle = {
-    fontSize: '30px',
-    margin: '0 10px',
-    fontWeight: 'normal'
-  };
-
-  const firstTextStyle = {
-    ...textStyle,
-    marginLeft: '20px'
-  };
-
-  const lastTextStyle = {
-    ...textStyle,
-    marginRight: '20px'
-  };
-
   return (
-    <div style={navStyle}>
-      <img src={groupImage} alt="Group logo" style={imgStyle} />
-      <h1 style={firstTextStyle}>DASHBOARD</h1>
-      <h1 style={textStyle}>DUTY CHART</h1>
-      <h1 style={textStyle}>ADD/REMOVE STAFF</h1>
-      <h1 style={lastTextStyle}>STAFF MEMBERS</h1>
-      <img src={userimage} alt="User icon" style={imgStyle} />
+    <>
+      <div className="nav">
+      <img src={groupImage} alt="Group logo" /> {/* Use the imported variable */}
+      <h1 className='h1'>DASHBOARD</h1>
+      <h1 className='h1'>DUTY CHART</h1>
+      <h1 className='h1'>ADD/REMOVE STAFF</h1>
+      <h1 className='h1'>STAFF MEMBERS</h1>
+      <img src={userimage} alt="User icon" />
     </div>
-  );
+      </>
+  )
 }
 
-export default Header;
+export default Header
