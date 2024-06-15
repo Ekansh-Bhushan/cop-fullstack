c30064730ea099a9a61a62847765f5979cc0e367
Ekansh <129383527+Ekansh-Bhushan@users.noreply.github.com>
2024-06-15 15:32:08 +0530

    theek hua

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import StaffManagement from './components/StaffManagement.js';
-
+import Final from './components/Login/login.js'
 import DutyTask from './components/DutyTask.js';
 import LoginScreen from './components/Login/LoginScreen.js';
 import ProfileScreen from './components/profileScreen.js';
@@ -12,10 +12,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginScreen />} />
+        <Route path="/" element={<Final />} />
         <Route path="/StaffManagement" element={<StaffManagement />} />
         <Route path="/DutyTask" element={<DutyTask />} />
-        {/* <Route path="/LoginScreen" element={<LoginScreen />} /> */}
+        <Route path="/LoginScreen" element={<LoginScreen />} />
         <Route path="/profileScreen" element={<ProfileScreen />} />
       </Routes>
     </Router>
