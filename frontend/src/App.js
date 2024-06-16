2ae82ce0fb4f3bf1c7b3034879d1c2a1565e81be
Ritik <ritikpal098@gmail.com>
2024-06-16 21:53:09 +0530

    fixing navbar size

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,6 @@ import DutyTask from "./components/dutychart/DutyTask.js";
 // import LoginScreen from './components/Login/LoginScreen.js';
 import ProfileScreen from "./components/P.js"; // Correct casing
 import Dashboard from "./components/dashboard/Dashboard.js";
-import Header from "./components/Header/header.js";
 // import Profiles from './components/P.js'; // Correct casing
 
 function App() {
@@ -31,8 +30,10 @@ function App() {
         <Route path="/StaffMembers" element={<StaffMembers />} />
         <Route path="/DutyTask" element={<DutyTask />} />
         <Route path="/Dashboard" element={<Dashboard />} />
-        <Route path="/Profile" element={<ProfileScreen/>} /> {/* Pass police as a prop */}
-        <Route path="/homepage" element={<Dashboard/>} /> {/* Pass police as a prop */}
+        <Route path="/Profile" element={<ProfileScreen />} />{" "}
+        {/* Pass police as a prop */}
+        <Route path="/homepage" element={<Dashboard />} />{" "}
+        {/* Pass police as a prop */}
       </Routes>
     </Router>
   );
