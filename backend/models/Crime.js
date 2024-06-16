73fb251f1830e398d10038be056ec14f5eac5c4f
Ritik <ritikpal098@gmail.com>
2024-06-16 22:32:55 +0530

    changing crime schema

--- backend/models/Crime.js
+++ backend/models/Crime.js
@@ -1,27 +1,31 @@
 // models/Crime.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const CrimeSchema = new mongoose.Schema({
-    latitude: {
-        type: Number,
-        required: true
-    },
-    longitude: {
-        type: Number,
-        required: true
-    },
-    typeOfCrime: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: String,
-        required: true
-    },
-    month: {
-        type: String,
-        required: true
-    }
+  lat: {
+    type: String,
+    required: true,
+  },
+  long: {
+    type: String,
+    required: true,
+  },
+  crime: {
+    type: String,
+    required: true,
+  },
+  beat: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  month: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('Crime', CrimeSchema);
+module.exports = mongoose.model("Crime", CrimeSchema);
