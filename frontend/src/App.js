fe1a23136767d2d8c248b4096b5381688c8b2b82
Ritik <ritikpal098@gmail.com>
2024-06-16 17:17:21 +0530

    creating dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import "./App.css";
 
-
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import './App.css';
-
-import StaffManagement from './components/addremovestaff/StaffManagement.js'
-import StaffMembers from './components/staff/StaffMembers.js';
-import Final from './components/Login/login.js';
-import DutyTask from './components/dutychart/DutyTask.js';
+import StaffManagement from "./components/addremovestaff/StaffManagement.js";
+import StaffMembers from "./components/staff/StaffMembers.js";
+import Final from "./components/Login/login.js";
+import DutyTask from "./components/dutychart/DutyTask.js";
 // import LoginScreen from './components/Login/LoginScreen.js';
-import ProfileScreen from './components/P.js'; // Correct casing
+import ProfileScreen from "./components/P.js"; // Correct casing
+import Dashboard from "./components/dashboard/Dashboard.js";
+import Header from "./components/Header/header.js";
 // import Profiles from './components/P.js'; // Correct casing
 
 function App() {
-  
   // Sample police data to pass to the Profile component
   const police = {
     name: "John Doe",
     role: "Sergeant",
     post: "Downtown",
     dob: "1980-01-01",
-    under: "Central Station"
+    under: "Central Station",
   };
 
   return (
@@ -32,8 +30,10 @@ function App() {
         <Route path="/StaffManagement" element={<StaffManagement />} />
         <Route path="/StaffMembers" element={<StaffMembers />} />
         <Route path="/DutyTask" element={<DutyTask />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
         {/* <Route path="/LoginScreen" element={<LoginScreen />} /> */}
-        <Route path="/Profile" element={<ProfileScreen/>} /> {/* Pass police as a prop */}
+        <Route path="/Profile" element={<ProfileScreen />} />{" "}
+        {/* Pass police as a prop */}
       </Routes>
     </Router>
   );
