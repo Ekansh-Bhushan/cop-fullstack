9f8a8e0263ca5c5dc05693b3411ae4c2fe3975d5
Ritik <ritikpal098@gmail.com>
2024-06-16 21:56:04 +0530

    fixing navbar size

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,11 +10,8 @@ import DutyTask from "./components/dutychart/DutyTask.js";
 // import LoginScreen from './components/Login/LoginScreen.js';
 import ProfileScreen from "./components/P.js"; // Correct casing
 import Dashboard from "./components/dashboard/Dashboard.js";
-<<<<<<< HEAD
-=======
 import Header from "./components/Header/header.js";
 import Crime from "./components/crimes/crime.js";
->>>>>>> 2576c4f2046dd82549d620a20dc3ee6a8b10da44
 // import Profiles from './components/P.js'; // Correct casing
 
 function App() {
@@ -35,16 +32,12 @@ function App() {
         <Route path="/StaffMembers" element={<StaffMembers />} />
         <Route path="/DutyTask" element={<DutyTask />} />
         <Route path="/Dashboard" element={<Dashboard />} />
-<<<<<<< HEAD
         <Route path="/Profile" element={<ProfileScreen />} />{" "}
         {/* Pass police as a prop */}
         <Route path="/homepage" element={<Dashboard />} />{" "}
         {/* Pass police as a prop */}
-=======
-        <Route path="/Profile" element={<ProfileScreen/>} /> {/* Pass police as a prop */}
-        <Route path="/homepage" element={<Dashboard/>} /> {/* Pass police as a prop */}
-        <Route path="/crimeEntry" element={<Crime/>} /> {/* Pass police as a prop */}
->>>>>>> 2576c4f2046dd82549d620a20dc3ee6a8b10da44
+        <Route path="/crimeEntry" element={<Crime />} />{" "}
+        {/* Pass police as a prop */}
       </Routes>
     </Router>
   );
