a33aa4ebab8bd5ae84465f3de760839cbd906e3c
Ekansh <129383527+Ekansh-Bhushan@users.noreply.github.com>
2024-08-01 14:20:59 +0530

    push

--- frontend/src/components/config/config.js
+++ frontend/src/components/config/config.js
@@ -1,7 +1,3 @@
-// export const API_URL = "http://93.127.172.217:5000/";
-<<<<<<< HEAD
-export const API_URL = "http://localhost:2005";
-=======
+export const API_URL = "http://93.127.172.217:2005/";
 // export const API_URL = "http://localhost:5000";
-export const API_URL = "https://dpwebssitebackend.onrender.com";
->>>>>>> dfe056f28f7fb5d1de09059dabfa7d40c39e7f75
+// export const API_URL = "https://dpwebssitebackend.onrender.com";
